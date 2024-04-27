7bb6cb605f43a975eec1db1df2da7b9e35503b0f
mxtlrr <ruson0914@gmail.com>
2024-04-27 12:49:03 -0500

    feat!: when user input enter, splits' data change

--- src/main.go
+++ src/main.go
@@ -4,17 +4,45 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
+
+	"github.com/mxtlrr/tinytimer/splits"
 )
 
 var (
 	seconds      int
 	milliseconds int
 	running      bool = true
+
+	/* For splits */
+	splits_     []splits.Split_t
+	split_count int
+	iterator    int = 0
 )
 
 func main() {
-	for running != false {
+	// Argv[1] is going to be the flag, either being
+	// --file, or --log.
+	var file string = "splits.txt"
+	if len(os.Args) > 1 {
+		args := os.Args
+		// We're able to change the file, but we're gonna read
+		// from this file
+		if strings.Compare(args[1], "--file") == 0 {
+			file = args[2]
+		}
+	}
+	splits_ = splits.Gen_splits(file)
+	fmt.Printf("found splits...\n")
+	for i := range splits_ {
+		fmt.Printf("%d: %s\n", i, splits_[i].NAME)
+	}
+
+	// Set split count
+	split_count = len(splits_)
+
+	for running {
 		if milliseconds < 1000 {
 			milliseconds++
 		} else {
@@ -26,20 +54,36 @@ func main() {
 		go checkInput()
 	}
 	fmt.Printf("\n")
+
+	// TODO: show output if argv[1] not log
+	for i := range splits_ {
+		fmt.Printf("SPLIT: %s | TIME %d.%d\n",
+			splits_[i].NAME, splits_[i].TIME_SECONDS,
+			splits_[i].TIME_MILLISEC)
+	}
+	exec.Command("stty", "-F", "/dev/tty", "echo").Run()
 }
 
 /* Check for input. Set running to false if we get input */
 func checkInput() {
-	// Set up terminal to not echo out character we get!
-	/* TODO: make it so it's usable on windows */
+	/* Hide enter
+	 * TODO: cross-platform */
 	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
 
-	var b []byte = make([]byte, 1)
-	for {
-		os.Stdin.Read(b)
-		if string(b) != "" {
-			running = false // Input!
-		}
+	if iterator == split_count {
+		running = false
+		iterator = 0
 	}
+	var (
+		n string
+	)
+	fmt.Scanf("%s", &n) // Input
 
+	// Set data for split[iterator]
+	// splits[iterator].NAME already set.
+	splits_[iterator].TIME_MILLISEC = milliseconds
+	splits_[iterator].TIME_SECONDS = seconds
+
+	iterator++
 }
